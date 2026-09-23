e94495479a4c0445ea20f860c934b6c7f6eb738f
markstevengray95-star <markstevengray95@gmail.com>
2026-09-23 12:37:28 +0100

    Cache extended-response practice offline

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE="waves-lab-v5";
+const CACHE="waves-lab-v6";
 const ASSETS=[
-  "./","./index.html","./styles.css","./advanced-features.css","./detail-upgrades.css","./course-upgrades-v3.css","./waves-data.js","./textbook-data.js","./app.js","./textbook.js","./advanced-features.js","./integration-patches.js","./textbook-expansion.js","./simulation-upgrades-v2.js","./simulation-upgrades-v3.js","./textbook-interactives-v3.js","./question-engine-v3.js","./physics-icon.svg","./manifest.webmanifest"
+  "./","./index.html","./styles.css","./advanced-features.css","./detail-upgrades.css","./course-upgrades-v3.css","./extended-response-v4.css","./waves-data.js","./textbook-data.js","./app.js","./textbook.js","./advanced-features.js","./integration-patches.js","./textbook-expansion.js","./simulation-upgrades-v2.js","./simulation-upgrades-v3.js","./textbook-interactives-v3.js","./question-engine-v3.js","./extended-response-v4.js","./physics-icon.svg","./manifest.webmanifest"
 ];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
